f3aeeacf997f4612ea46858d157e032dd72b3c0a
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-07 15:18:31 -0300

    convertendo response

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,48 @@
+import 'dart:convert';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
-void findAll() async {
-  final Response response = await get('http://192.168.1.105:8080/transactions');
-  print(response.body);
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+
 }
+
+
+Future<List<Transaction>> findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
+
+  final Response response =
+  await client.get('http://192.168.1.105:8080/transactions');
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+
+  for (Map<String, dynamic> transactionJson in decodedJson) {
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+  return transactions;
+  //debugPrint(decodedJson.toString());
+}
+
